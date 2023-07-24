28d6f5966412efd91335846dd4cdb7ba3ba1964b
Codepunk20072 <121000184+Codepunk20072@users.noreply.github.com>
2023-07-24 12:30:24 +0530

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
-import Header from "Header";
-import Footer from "Footer";
-import Note from "Note";
-import CreateArea from "CreateArea";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+import CreateArea from "./CreateArea";
 
 function App() {
   const [notes, setNotes] = useState([]);
